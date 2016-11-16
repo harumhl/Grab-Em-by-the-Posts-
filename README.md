f0db9523363bc3c8153c645473befecc0a0849b2
jarheadr176 <jarheadr176@tamu.edu>
2016-11-15 22:18:39 -0600

    Adding changes to tweepy_test and sample tweets

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 489Project
+
+
+Donald Trump's announcement for candidacy June 16, 2015
+
+Hillary's announcement for candidacy April 12, 2015
+
+
+Tweet Breakdown:
+https://dev.twitter.com/overview/api/tweets
--- tweepy_test.py
+++ tweepy_test.py
@@ -1,21 +1,40 @@
-import tweepy
+consumer_key='dO8qYUOk22LqPYQD5h6WZ3Cp2'
+consumer_secret='l8LOOvYM6BsyH0YNAvmvRerRbhQLO2qj1j0LlhfdTK0m14IVX1'
 
-#Major news outlets and their tweets: (oldest w)
-major_news_outlets = {'CNN':(0,1),
-                      'FoxNews':(0,1),
-                      'MSNBC':(0,1),
-                      'USATODAY':(0,1),
-                      'nytimes':(0,1),
-                      'wsj':(0,1),
-                      'cbsnews':(0,1)}
+access_token='1480581678-SxL799KNagjx2lOad43REX6e6dHIZFQqQpizyOB'
+access_token_secret='nTvOhlNytYiiK5O9DJaZnG7gTIAXxee9ANNSSSqt2sceV'
+
+import tweepy
+import time
+
+#major_news_outlets = ['CNN','FoxNews','MSNBC','USATODAY','nytimes','wsj','cbsnews']
+
+major_news_outlets = ['CNN']
 
 def limit_handled(cursor):
     while True:
         try:
             yield cursor.next()
         except tweepy.RateLimitError:
+            print "Waiting..."
             time.sleep(15 * 60)
+        except tweepy.TweepError:
+            print "Waiting..."
+            time.sleep(15 * 60)
+        except StopIteration as e:
+            print type(e)
+            return
+            #print "Exception: " + e.msg
 
+def process(unfiltered_json_tweet):
+    f = open("test.txt",'a')
+    f.write(str(unfiltered_json_tweet))
+    f.write("\n")
+
+    #return filtered_json_tweet
+
+def save(json_tweet):
+    return
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -23,20 +42,20 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-'''
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
-'''
 
-#api.user_timeline([id/user_id/screen_name][, since_id][, max_id][, count][, page])
+#limit_handled(tweepy.Cursor(api.search,q="since:2015-4-12 until:2016-11-14 donald OR trump OR hillary OR clinton from:CNN ")
 
+query_string = "since:2015-4-12 until:2016-11-14 donald OR trump OR hillary OR clinton from:"
 
-cnn_tweets = api.user_timeline('CNN')
-print type(cnn_tweets)
-for tweet in cnn_tweets:
-    print 'type: ' + str(type(tweet))
-    print tweet._json
+for news_outlet in major_news_outlets:
+    query_string += news_outlet
+    for tweet in limit_handled(tweepy.Cursor(api.search,q="since:2016-11-10 until:2016-11-14 donald OR trump OR hillary OR clinton from:CNN").items()):
+        print query_string
+        #print 'type: ' + str(type(tweet))
+        #print tweet._json
+        process(tweet._json)
+        #print filtered_tweet
+
 
 
 
