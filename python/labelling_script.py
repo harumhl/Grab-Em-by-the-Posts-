da87fb57641d671e03dbb0a59a7237cbb713f49d
jarheadr176 <jarheadr176@tamu.edu>
2016-12-04 14:48:20 -0600

    Fixing error in labelling script

--- python/labelling_script.py
+++ python/labelling_script.py
@@ -1,12 +1,13 @@
 def classify_post (row):
-   status = row['status_message'].lower()
+   status = row['content'].lower()
    if (('donald' in status) or ('trump' in status)) and (('hillary' in status) or ('clinton' in status)) :
       return 'other'
-   if ('donald' in status) or ('trump' in status) :
+   elif ('donald' in status) or ('trump' in status) :
       return 'trump'
-   if ('hillary' in status) or ('clinton' in status) :
+   elif ('hillary' in status) or ('clinton' in status) :
       return 'clinton'
-   return 'other'
+   else:
+      return 'other'
 
 cbs_df['candidate_label'] = cbs_df.apply (lambda row: classify_post (row),axis=1)
 cbs_df.head()
